72eee3d64719be366cc3b3985709eb55a925e07b
Jacob Hale <Jchale23@byui.edu>
2026-01-15 13:32:47 -0700

    Add project files.

---
+++ FoodItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Xml.Linq;
+
+namespace MIssion3Assignment
+{
+    internal class FoodItem
+    {
+        public string name;
+        public string category;
+        public int quantity;
+        public string expirationDate;
+        public FoodItem(string name, string category, int quantity, string expirationDate) 
+        {
+            this.name = name;
+            this.category = category;
+            this.quantity = quantity;
+            this.expirationDate = expirationDate;
+
+            // array of arrays to hold food item details
+        }
+
+        public List<string[]> AddFoodItem(List<string[]> list)
+        {
+            // code to add food item into array
+            string[] itemData = { name, category, quantity.ToString(), expirationDate };
+            list.Add(itemData);
+            return list;
+        }
+
+        public void DeleteFoodItem()
+        {
+            // code to delete food item from array
+            // enter name of food item to delete
+            // loop through array to find food item and delete it
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using MIssion3Assignment;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string[]> foodItems = new List<string[]>();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("1: Add Food Items");
+            Console.WriteLine("2: Delete Food Items");
+            Console.WriteLine("3: Print List of Current Food Items");
+            Console.WriteLine("4: Exit the Program");
+            Console.WriteLine("=====================================");
+            Console.Write("Enter the number of the function you want to do: ");
+            string choice = Console.ReadLine();
+            Console.WriteLine("=====================================");
+            switch (choice)
+            {
+                case "1":
+                    // Add Food Logic
+                    Console.Write("Enter the name of the food item: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter the category of " + name + ": ");
+                    string category = Console.ReadLine();
+                    Console.Write("Enter the quantity of " + name + ": ");
+                    int quantity = int.Parse(Console.ReadLine());
+                    Console.Write("Enter the expiration date of " + name + ": ");
+                    string expirationDate = Console.ReadLine();
+
+                    FoodItem item = new FoodItem(name, category, quantity, expirationDate);
+                    foodItems = item.AddFoodItem(foodItems);
+                    Console.WriteLine(name + " has been added to the list.");
+
+                    break;
+                case "2":
+                    // Delete Food Logic
+                    break;
+                case "3":
+                    // Print list of current food items
+                    break;
+                case "4":
+                    running = false;
+                    Console.WriteLine("Exiting the program now. Have a great day!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+
+        
+
+    }
+
+    //public Array PrintList()
+    //{
+    //    // code to print list of food items
+    //    return foodItems;
+
+    //}
+}
